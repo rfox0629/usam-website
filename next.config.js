642b1cc4044c584b5d9cbb42884e0566356dd8fa
“rfox0629” <rfox0629@gmail.com>
2026-04-04 23:29:19 -0500

    Fix structure and restore app

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: path.join(__dirname),
+  },
+};
+module.exports = nextConfig;
