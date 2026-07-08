4334752a6baef15e83cf85f144ac5e8465064732
rfox0629 <ryan@usamissionaries.org>
2026-07-08 11:25:32 -0500

    Fix DOS invite edit UUID validation

--- app/api/dos/app/table-invitations/route.ts
+++ app/api/dos/app/table-invitations/route.ts
@@ -36,7 +36,7 @@ function asStringArray(value: unknown) {
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(value);
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
 }
 
 function isMissingInviteModel(error: { message?: string } | null | undefined) {
